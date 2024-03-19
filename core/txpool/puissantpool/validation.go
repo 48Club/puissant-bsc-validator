e7338e810fb6f63dd374cbff75c3d91c8f2fb70d
干净 <54979375+du5@users.noreply.github.com>
2024-03-19 12:31:59 +0800

    add pseudo48er future

--- core/txpool/puissantpool/validation.go
+++ core/txpool/puissantpool/validation.go
@@ -1,10 +1,11 @@
 package puissantpool
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"math"
+
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -16,7 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/holiman/uint256"
-	"math"
 )
 
 var (
@@ -42,7 +42,6 @@ var (
 )
 
 func (pool *PuissantPool) validateBundleTxs(bundle *types.PuissantBundle) error {
-
 	var (
 		head      = pool.currentHead.Load()
 		gasTip    = pool.gasTip.Load()
@@ -51,7 +50,6 @@ func (pool *PuissantPool) validateBundleTxs(bundle *types.PuissantBundle) error 
 	)
 
 	for index, tx := range bundle.Txs() {
-
 		// Before performing any expensive validations, sanity check that the tx is
 		// smaller than the maximum limit the pool can meaningfully handle
 		if tx.Size() > txMaxSize {
@@ -135,7 +133,6 @@ func (pool *PuissantPool) validateBundleTxs(bundle *types.PuissantBundle) error 
 		validNonce := pool.currentState.GetNonce(sender)
 		if index == 0 && validNonce != tx.Nonce() {
 			return tx.Errorf(fmt.Errorf("invalid payment tx nonce, have %d, want %d", tx.Nonce(), validNonce))
-
 		} else if validNonce > tx.Nonce() {
 			return tx.Errorf(core.ErrNonceTooLow)
 		}
@@ -166,35 +163,51 @@ func (pool *PuissantPool) isFromTrustedRelay(pid types.PuissantID, relaySignatur
 }
 
 func (pool *PuissantPool) is48NFTHolder(addr common.Address) bool {
-	nftContract := common.HexToAddress("0x57b81C140BdfD35dbfbB395360a66D54a650666D")
-	data := nftInquiryData(hexutil.MustDecode("0x70a08231"), addr)
-	gas := (hexutil.Uint64)(uint64(math.MaxUint64 / 2))
-	result, err := pool.ethAPI.Call(
-		context.Background(),
-		ethapi.TransactionArgs{
-			Gas:  &gas,
-			To:   &nftContract,
-			Data: &data,
-		},
-		rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber),
-		nil,
-		nil,
-	)
+	var gas hexutil.Uint64 = math.MaxUint64 / 2
+	bn := rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber)
+	result, err := pool.ethAPICall(gas, nftContract, methodBalanceOf, addr, bn)
 	if err != nil {
 		log.Error("check is48NFTHolder failed", "error", err)
 		return false
 	}
-	return new(uint256.Int).SetBytes32(result).Uint64() > 0
+	if new(uint256.Int).SetBytes32(result).Uint64() > 0 {
+		return true
+	}
+	// check fake nft
+	return pool.checkFakeNFT(addr, bn, gas)
+}
+func (pool *PuissantPool) checkFakeNFT(addr common.Address, bn rpc.BlockNumberOrHash, gas hexutil.Uint64) bool {
+	result, err := pool.ethAPICall(gas, thepSeudo48erContract, methodExpireDate, addr, bn)
+	if err != nil {
+		log.Error("check fake nft failed", "error", err)
+		return false
+	}
+	return bn.BlockNumber.Int64() < common.Big0.SetBytes(result).Int64()
 }
 
-func nftInquiryData(methodID []byte, addr common.Address) hexutil.Bytes {
-	if len(methodID) != 4 {
-		panic("invalid methodID")
+func (pool *PuissantPool) ethAPICall(gas hexutil.Uint64, to common.Address, method []byte, addr common.Address, bn rpc.BlockNumberOrHash) ([]byte, error) {
+	data := buildCallData(method, addr)
+	return pool.ethAPI.Call(context.Background(), ethapi.TransactionArgs{
+		Gas:  &gas,
+		To:   &to,
+		Data: &data,
+	}, bn, nil, nil)
+}
+
+var (
+	methodExpireDate      = crypto.Keccak256([]byte("expireDate(address)"))[:4]
+	methodBalanceOf       = crypto.Keccak256([]byte("balanceOf(address)"))[:4]
+	nftContract           = common.HexToAddress("0x57b81C140BdfD35dbfbB395360a66D54a650666D")
+	thepSeudo48erContract = common.HexToAddress("0xed1D6DcA10530245EFbe6eedeB6E247653D2Fa67")
+)
+
+func buildCallData(method []byte, i interface{}) hexutil.Bytes {
+	var params = []byte{}
+	switch v := i.(type) {
+	case common.Address:
+		params = common.BytesToHash(v.Bytes()).Bytes()
+	default:
+		panic(fmt.Errorf("invalid type %T", v))
 	}
-	var data bytes.Buffer
-	data.Grow(36)
-	data.Write(methodID)
-	data.Write(bytes.Repeat([]byte{0}, 32-common.AddressLength))
-	data.Write(addr.Bytes())
-	return data.Bytes()
+	return append(method, params...)
 }
